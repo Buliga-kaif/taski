505a378926edae24ca1f54be8febd5f4a3b52ced
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2024-11-30 14:40:34 +0300

    Update 2.3.c

--- 2.3.c
+++ 2.3.c
@@ -1,54 +1,35 @@
-#define _USE_MATH_DEFINES
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>  
+#include <locale.h>
+#include <stdbool.h>
 
-/*
- * @brief Функция ввода числа типа double с консоли.
- * @return Возвращает введенное пользователем положительное число типа double.
- */
-double inputDouble() {
-    double number = 0.0; // Инициализация переменной number значением 0.0
-    while (scanf("%lf", &number) != 1 || number <= 0) {
-        printf("Ошибка ввода. Пожалуйста, введите положительное число: ");
-        while (getchar() != '\n'); // Очистка буфера ввода
-    }
-    return number;
-}
+// Прототипы функций
+double inputDouble(void);
+bool canPass(double x, double y, double z, double r, double s);
 
-/*
- * @brief Точка входа в программу.
- * @return Возвращает 0 в случае успешного выполнения программы.
- */
-int main() {
+int main(void) {
     setlocale(LC_ALL, "");
-    double x, y, z; 
-    double r, s;    
-    // Ввод размеров кирпича
+
     printf("Введите размеры кирпича (x, y, z): \n");
 
     printf("x: ");
-    x = inputDouble();
+    double x = inputDouble();
 
     printf("y: ");
-    y = inputDouble();
+    double y = inputDouble();
 
     printf("z: ");
-    z = inputDouble();
+    double z = inputDouble();
 
-    // Ввод размеров отверстия
     printf("Введите размеры отверстия (r, s): \n");
 
     printf("r: ");
-    r = inputDouble();
+    double r = inputDouble();
 
     printf("s: ");
-    s = inputDouble();
+    double s = inputDouble();
 
-    // Проверка возможности прохождения кирпича через отверстие
-    if ((x <= r && y <= s) || (x <= s && y <= r) ||
-        (x <= r && z <= s) || (x <= s && z <= r) ||
-        (y <= r && z <= s) || (y <= s && z <= r)) {
+    if (canPass(x, y, z, r, s)) {
         printf("Кирпич пройдет через отверстие.\n");
     }
     else {
@@ -56,4 +37,19 @@ int main() {
     }
 
     return 0;
+}
+
+double inputDouble(void) {
+    double number = 0.0;
+    while (scanf("%lf", &number) != 1 || number <= 0) {
+        printf("Ошибка ввода. Пожалуйста, введите положительное число: ");
+        while (getchar() != '\n');
+    }
+    return number;
+}
+
+bool canPass(double x, double y, double z, double r, double s) {
+    return (x <= r && y <= s) || (x <= s && y <= r) ||
+        (x <= r && z <= s) || (x <= s && z <= r) ||
+        (y <= r && z <= s) || (y <= s && z <= r);
 }
