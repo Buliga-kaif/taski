9d842439af5b654e3b6ac637859ee2d1f75bc246
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2024-11-30 14:54:22 +0300

    Update 3.1.c

--- 3.1.c
+++ 3.1.c
@@ -7,19 +7,6 @@
 #include <float.h>
 
 /*
- * @brief Функция ввода числа типа double с консоли.
- * @return Возвращает введенное с консоли число типа double.
- */
-double inputDouble() {
-    double number = 0.0;
-    while (scanf("%lf", &number) != 1) {
-        printf("Input error. Please enter a number: ");
-        while (getchar() != '\n');
-    }
-    return number;
-}
-
-/*
  * @brief Функция для вычисления значения функции y при аргументе x.
  * @param x Значение аргумента x.
  * @return Результат вычисления значения функции y при аргументе x.
@@ -34,10 +21,23 @@ double y(const double x) {
  * @param rightBoarder Значение правой границы интервала.
  */
 void checkBoardersOfInterval(const double leftBoarder, const double rightBoarder) {
-    if (leftBoarder >= rightBoarder) {
-        printf("Input error (the left border should be smaller than the right one)\n");
+    if (leftBoarder > rightBoarder) {
+        printf("Input error (the left border should not be greater than the right one)\n");
         exit(EXIT_FAILURE);
     }
+}
+
+/*
+ * @brief Функция ввода числа типа double с консоли.
+ * @return Возвращает введенное с консоли число типа double.
+ */
+double inputDouble() {
+    double number = 0.0;
+    while (scanf("%lf", &number) != 1) {
+        printf("Input error. Please enter a number: ");
+        while (getchar() != '\n');
+    }
+    return number;
 }
 
 /*
@@ -46,31 +46,33 @@ void checkBoardersOfInterval(const double leftBoarder, const double rightBoarder
  * @return Результат проверки существования функции y при аргументе x.
  */
 bool isFunctionExists(const double x) {
-    double value = y(x);
-    return isfinite(value);
+    return isfinite(y(x));
 }
 
 /*
  * @brief Точка входа в программу.
  * @return Возвращает 0 в случае успешного выполнения программы.
  */
-int main() {
-    
+int main(void) {
     printf("Enter the left border of the interval:\t");
     const double leftBoarder = inputDouble();
+
     printf("Enter the right border of the interval:\t");
     const double rightBoarder = inputDouble();
     checkBoardersOfInterval(leftBoarder, rightBoarder);
 
     printf("Enter the interval step:\t");
-    const double dx = inputDouble();
+    double dx;
+    while ((dx = inputDouble()) <= 0.0) {
+        printf("Step must be positive. Please enter again:\t");
+    }
 
     printf("x\t\t y\n");
     printf("-----------------------\n");
 
-    for (double x = leftBoarder; x <= rightBoarder; x += dx) {
+    for (double x = leftBoarder; x <= rightBoarder + DBL_EPSILON; x += dx) {
         if (!isFunctionExists(x))
-            printf("%.2lf\t It is impossible to calculat\n", x);
+            printf("%.2lf\t It is impossible to calculate\n", x);
         else
             printf("%.2lf\t %.5lf\n", x, y(x));
     }
