708669166de32d84a95ec9e1c0c1be5d6f9f1189
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2024-11-30 14:33:30 +0300

    Update 2.2.c

--- 2.2.c
+++ 2.2.c
@@ -5,46 +5,13 @@
 #include <locale.h>  
 
 /*
- * @brief Функция для ввода числа типа double с консоли.
- *
- * @return Возвращает введённое пользователем число типа double.
- */
-double inputDouble() {
-    double number = 0.0; 
-    while (scanf("%lf", &number) != 1) {
-        printf("Неверный ввод. Пожалуйста, введите число: ");
-        while (getchar() != '\n'); 
-    }
-    return number;
-}
-
-/*
- * @brief Функция для вычисления значения функции y в зависимости от аргумента x и параметра a.
- *
- * @param x Значение аргумента функции.
- * @param a Параметр a, используемый в вычислениях.
- * @return Значение y, вычисленное на основе введённых x и a.
- */
-double calculateY(const double x, const double a) {
-    if (x >= 1 && x <= 2) {
-        return a * pow(x, 2) * log(x);
-    }
-    else if (x < 1) {
-        return 1;
-    }
-    else { // x > 2
-        return exp(a * x) * cos(x);
-    }
-}
-
-/*
  * @brief Главная функция программы.
  *
  * Основная цель программы — вычисление значения функции y на основе заданного x.
  *
  * @return Возвращает 0 в случае успешного выполнения программы.
  */
-int main() {
+int main(void) {
     setlocale(LC_ALL, "");
     const double a = 2.0; 
     printf("Введите значение x: ");
@@ -53,5 +20,34 @@ int main() {
     printf("Результат y = %lf\n", y); 
     return 0;
 }
-
-
+/*
+ * @brief Функция для ввода числа типа double с консоли.
+ *
+ * @return Возвращает введённое пользователем число типа double.
+ */
+double inputDouble(void) {
+    double number = 0.0; 
+    while (scanf("%lf", &number) != 1) {
+        printf("Неверный ввод. Пожалуйста, введите число: ");
+        while (getchar() != '\n'); 
+    }
+    return number;
+}
+/*
+ * @brief Функция для вычисления значения функции y в зависимости от аргумента x и параметра a.
+ *
+ * @param x Значение аргумента функции.
+ * @param a Параметр a, используемый в вычислениях.
+ * @return Значение y, вычисленное на основе введённых x и a.
+ */
+double calculateY(const double x, const double a) {
+       if  (x > 2)
+        return exp(a * x) * cos(x);
+    }
+    else if (x < 1) {
+        return 1;
+    }
+    else { 
+        return a * pow(x, 2) * log(x);
+    }
+}
