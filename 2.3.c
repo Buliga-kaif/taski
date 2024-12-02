99519fbd137223a0c73e41c09fa527a5b4531b20
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2024-12-02 16:36:49 +0300

    Update 2.3.c

--- 2.3.c
+++ 2.3.c
@@ -2,10 +2,31 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <stdbool.h>
+#include <float.h> 
 
-// Прототипы функций
+/*
+ * @brief Главная функция программы.
+ *
+ * Программа запрашивает размеры кирпича и отверстия, и определяет,
+ * сможет ли кирпич пройти через отверстие.
+ *
+ * @return Возвращает 0 в случае успешного выполнения программы.
+ */
 double inputDouble(void);
+/*
+ * @brief Функция для проверки возможности прохождения кирпича через отверстие.
+ * @return Возвращает true, если кирпич может пройти через отверстие, иначе false.
+ */
 bool canPass(double x, double y, double z, double r, double s);
+/*
+ * @brief Главная функция программы.
+ *
+ * Программа запрашивает размеры кирпича (x, y, z) и отверстия (r, s),
+ * после чего определяет, может ли кирпич пройти через отверстие.
+ * Пользователь вводит размеры с консоли.
+ * @return Возвращает 0 в случае успешного выполнения программы.
+ */
+
 
 int main(void) {
     setlocale(LC_ALL, "");
@@ -39,14 +60,20 @@ int main(void) {
     return 0;
 }
 
+/*
+ * @brief Функция для ввода числа типа double с консоли с проверкой корректности.
+ *
+ * @return Возвращает введённое пользователем положительное число типа double.
+ */
 double inputDouble(void) {
     double number = 0.0;
-    while (scanf("%lf", &number) != 1 || number <= 0) {
+    while (scanf("%lf", &number) != 1 || number <= DBL_EPSILON) {
         printf("Ошибка ввода. Пожалуйста, введите положительное число: ");
-        while (getchar() != '\n');
+        while (getchar() != '\n'); 
     }
     return number;
 }
+
 
 bool canPass(double x, double y, double z, double r, double s) {
     return (x <= r && y <= s) || (x <= s && y <= r) ||
