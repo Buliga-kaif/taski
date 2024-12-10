6a448f1ae104888c8495b10ef65e5a1ec50bf290
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2024-12-10 18:07:43 +0300

    Update 1-1.c

--- 1-1.c
+++ 1-1.c
@@ -18,7 +18,12 @@ double getA(const double x, const double y);
  * @return Вычисленное значение по формуле.
  */
 double getB(const double x,const  double y,const double z);
-
+/*
+ * @brief Главная функция программы.
+ * Функция main инициализирует значения переменных x, y, z и вычисляет результаты функций getA и getB.
+ * Затем выводит значения x, y, z, результат getA и результат getB на стандартный вывод.
+ * @return Возвращает 0 при успешном завершении программы.
+ */
 int main() {
     const double x = 1.426;
     const double y = -1.22;
