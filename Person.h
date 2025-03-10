6701095631f17ceebc3e09b54905dba300ede2ce
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-03-10 14:46:11 +0300

    Create Person.h

---
+++ Person.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Person {
+private:
+    string lastName;
+    string firstName;
+    string patronymic;
+
+public:
+
+    Person();
+    Person(const string& ln, const string& fn, const string& pn = "");
+
+
+    string getLastName() const;
+    string getFirstName() const;
+    string getPatronymic() const;
+};
