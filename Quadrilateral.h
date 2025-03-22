8db6f0aa1d28e94c86720cb7eb56483d83c7de09
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-03-22 18:36:34 +0300

    Create Quadrilateral.h

---
+++ Quadrilateral.h
@@ -0,0 +1,20 @@
+#ifndef QUADRILATERAL_H
+#define QUADRILATERAL_H
+
+#include <vector>
+#include "Point.h"
+
+class Quadrilateral {
+private:
+    std::vector<Point> vertices;
+
+    double crossProduct(const Point& p1, const Point& p2, const Point& p3);
+
+public:
+    Quadrilateral(const std::vector<Point>& points);
+
+    bool isConvex();
+    bool canCircumscribe();
+};
+
+#endif // QUADRILATERAL_H
