68dde4dbca9af5eacb03dc1943ed573a857c1dc8
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-04-20 14:26:48 +0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -3,5 +3,5 @@
 Point::Point(float x, float y) : x(x), y(y) {}
 
 void Point::display() const {
-    std::cout << "Point(" << x << ", " << y << ")\n";
+    std::cout << "Point(" << x << ", " << y << ")\n"; 
 }
