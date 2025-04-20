d791efb68d106d1704384fdc1104618e9ccc8872
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-04-20 14:28:58 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -3,7 +3,9 @@
 class Point {
 private:
     float x, y;
-
+public:
     Point(float x = 0, float y = 0);
     void display() const;
+    float getX() const { return x; }
+    float getY() const { return y; }
 };
