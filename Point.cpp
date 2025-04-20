2625b63a3a5dc3d3839cb54ef9c87207725c2290
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-04-20 14:18:29 +0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -4,6 +4,3 @@ Point::Point(double x, double y) : x(x), y(y) {}
 
 double Point::getX() const { return x; }
 double Point::getY() const { return y; }
-
-void Point::setX(double newX) { x = newX; }
-void Point::setY(double newY) { y = newY; }
