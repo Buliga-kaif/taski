0357bb076d388211b8baa4714441b2e697e1b1c1
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-04-20 14:30:56 +0300

    Update Segment.cpp

--- Segment.cpp
+++ Segment.cpp
@@ -1,28 +1,31 @@
 #include "Segment.h"
 
 Segment::Segment(const Point& left, const Point& right)
-    : left_point(left), right_point(right.x, left.y) {}
+    : left_point(left), right_point(right) {} 
 
 float Segment::calculate_ordinate(float x) const {
-    if (x >= left_point.x && x <= right_point.x) {
-        return left_point.y;
-    } else {
-        throw std::out_of_range("x is out of the segment bounds");
-    }
+    if (x >= left_point.getX() && x <= right_point.getX()) {
+        return left_point.getY(); // Сегмент горизонтальный (y одинаковый)
+    } 
+    throw std::out_of_range("x is out of the segment bounds");
 }
 
 void Segment::shift_left(float delta) {
-    left_point.x -= delta;
-    right_point.x -= delta;
+    left_point = Point(left_point.getX() - delta, left_point.getY());
+    right_point = Point(right_point.getX() - delta, right_point.getY());
 }
 
-Segment Segment::read_segment(float left_x, float left_y, float right_x) {
-    Point left(left_x, left_y);
-    return Segment(left, Point(right_x, 0));
+Segment Segment::read_segment(float left_X, float left_y, float right_X) {
+    Point left(left_X, left_y);
+    Point right(right_X, left_y); 
+    return Segment(left, right);
 }
+
 
 void Segment::display() const {
     std::cout << "Segment: ";
     left_point.display();
+    std::cout << " -> ";
     right_point.display();
+    std::cout << "\n";
 }
