ff0ed82474bea6710818779e1d695884af13b10a
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-04-24 14:20:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include "Segment.h"
 #include <iostream>
 
-int main() { 
-    float x1, y1, x2;
-    std::cout << "Enter left X, left Y, right X: ";
-    std::cin >> x1 >> y1 >> x2; 
-    Segment segment = Segment::read_segment(x1, y1, x2);
+int main() {
+    float x1, y1, x2, y2;
+    std::cout << "Enter left X, left Y, right X, right Y: ";
+    std::cin >> x1 >> y1 >> x2 >> y2;
+
+    Segment segment = Segment::read_segment(x1, y1, x2, y2);
     segment.display();
 
     try {
         float x;
         std::cout << "Enter x to calculate ordinate: ";
-        std::cin >> x; 
-        std::cout << "ordinate at x=" << x << ": " 
+        std::cin >> x;
+        std::cout << "Ordinate at x=" << x << ": " 
                   << segment.calculate_ordinate(x) << std::endl;
     } catch (const std::out_of_range& e) {
         std::cerr << e.what() << std::endl;
@@ -20,4 +21,6 @@ int main() {
 
     segment.shift_left(1);
     segment.display();
-    return 0; 
+
+    return 0;
+}
