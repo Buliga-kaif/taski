63a52cca2d522d5480c9676cd6be99946eead644
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-04-24 14:19:35 +0300

    Update Segment.h

--- Segment.h
+++ Segment.h
@@ -1,15 +1,19 @@
+#ifndef SEGMENT_H
+#define SEGMENT_H
+
 #include "Point.h"
 #include <stdexcept>
 
 class Segment {
 private:
-    Point left_point;
-    Point right_point;
+    Point left_point, right_point;
 
 public:
     Segment(const Point& left, const Point& right);
     float calculate_ordinate(float x) const;
     void shift_left(float delta);
-    static Segment read_segment(float left_x, float left_y, float right_x);
+    static Segment read_segment(float left_x, float left_y, float right_x, float right_y);
     void display() const;
 };
+
+#endif
