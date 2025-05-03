d2c35a0e962f7a06e4a7d515ec727e1d004c5816
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-05-03 11:39:54 +0300

    Update Quadrilateral.h

--- Quadrilateral.h
+++ Quadrilateral.h
@@ -7,13 +7,19 @@
 class Quadrilateral {
 private:
     std::vector<Point> vertices;
-    double crossProduct(const Point& p1, const Point& p2, const Point& p3) const;
-    double getSideLength(size_t i, size_t j) const;
+    
+    struct Line {
+        double A, B, C; // Ax + By + C = 0
+    };
 
+    Line getLineFromPoints(const Point& p1, const Point& p2) const;
+    bool findIntersection(const Line& l1, const Line& l2, Point& intersection) const;
+    bool isPointInsideBoundingBox(const Point& p) const;
+    
 public:
     Quadrilateral(const std::vector<Point>& points);
     bool isConvex() const;
-    bool canCircumscribe() const;
+    bool canCircumscribe(double tolerance = 1e-9) const;
 };
 
 #endif
