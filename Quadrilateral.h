5aa3933c0cdc0f8b165063ebdc9a14afad85af32
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-05-03 11:42:38 +0300

    Update Quadrilateral.h

--- Quadrilateral.h
+++ Quadrilateral.h
@@ -7,15 +7,18 @@
 class Quadrilateral {
 private:
     std::vector<Point> vertices;
-    
+
     struct Line {
-        double A, B, C;
+        double A, B, C; 
     };
 
     Line getLineFromPoints(const Point& p1, const Point& p2) const;
     bool findIntersection(const Line& l1, const Line& l2, Point& intersection) const;
     bool isPointInsideBoundingBox(const Point& p) const;
-    
+    double crossProduct(const Point& p1, const Point& p2, const Point& p3) const;
+    bool areCollinear(const Point& a, const Point& b, const Point& c) const;
+    double getSideLength(size_t i, size_t j) const;
+
 public:
     Quadrilateral(const std::vector<Point>& points);
     bool isConvex() const;
