80d851fab3a19891ea6a4c331d93066a73b6e014
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-05-03 11:52:01 +0300

    Update Quadrilateral.h

--- Quadrilateral.h
+++ Quadrilateral.h
@@ -4,12 +4,20 @@
 #include <vector>
 #include "Point.h"
 
+/**
+ * @class Quadrilateral
+ * @brief Представляет четырёхугольник и предоставляет методы для анализа его свойств.
+ */
 class Quadrilateral {
 private:
     std::vector<Point> vertices;
 
+    /**
+     * @struct Line
+     * @brief Уравнение прямой в форме Ax + By + C = 0.
+     */
     struct Line {
-        double A, B, C; 
+        double A, B, C;
     };
 
     Line getLineFromPoints(const Point& p1, const Point& p2) const;
@@ -20,8 +28,27 @@ private:
     double getSideLength(size_t i, size_t j) const;
 
 public:
+    /**
+     * @brief Конструктор четырёхугольника.
+     * @param points Вектор из 4 точек.
+     * @throws std::invalid_argument:
+     * - Если точек не 4.
+     * - Если точки совпадают.
+     * - Если три точки коллинеарны.
+     */
     Quadrilateral(const std::vector<Point>& points);
+    
+    /**
+     * @brief Проверяет выпуклость четырёхугольника.
+     * @return true, если выпуклый.
+     */
     bool isConvex() const;
+    
+    /**
+     * @brief Проверяет возможность описания окружности.
+     * @param tolerance Допустимая погрешность (по умолчанию 1e-9).
+     * @return true, если условие Птолемея выполняется.
+     */
     bool canCircumscribe(double tolerance = 1e-9) const;
 };
 
