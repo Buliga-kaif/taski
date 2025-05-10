f8517e78760b79c42d34e870b02f8a309abb1ae0
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-05-10 11:13:56 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,11 +1,17 @@
+#ifndef POINT_H
+#define POINT_H
+
 #include <iostream>
 
 class Point {
 private:
     float x, y;
+
 public:
     Point(float x = 0, float y = 0);
     void display() const;
     float getX() const;
     float getY() const;
 };
+
+#endif
