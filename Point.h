6cde610e2c39384753f21d7532d9ad6427993c49
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-05-19 12:54:37 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,17 +1,21 @@
+
 #ifndef POINT_H
 #define POINT_H
 
-#include <iostream>
+#include <limits>
 
 class Point {
 private:
     float x, y;
+    static constexpr float EPSILON = std::numeric_limits<float>::epsilon();
 
 public:
     Point(float x = 0, float y = 0);
-    void display() const;
     float getX() const;
     float getY() const;
+    void display() const;
+    bool operator==(const Point& other) const;
+    bool operator!=(const Point& other) const;
 };
 
 #endif
