285053d4a58cfb43b17a474fd6d4299f239b0f68
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-05-19 12:55:21 +0300

    Update Segment.h

--- Segment.h
+++ Segment.h
@@ -8,6 +8,7 @@
 class Segment {
 private:
     Point left_point, right_point;
+    static constexpr float EPSILON = std::numeric_limits<float>::epsilon();
 
 public:
     Segment(const Point& left, const Point& right);
@@ -15,9 +16,8 @@ public:
     void shift_left(float delta);
     static Segment read_segment(std::istream& is);
     void display() const;
-
-    const Point& getLeft() const { return left_point; }
-    const Point& getRight() const { return right_point; }
+    const Point& getLeft() const;
+    const Point& getRight() const;
 };
 
 #endif
