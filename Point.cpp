3c6f4f46e2560517b48494d40485769f593c02a0
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-05-19 12:54:57 +0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,11 +1,21 @@
 #include "Point.h"
+#include <cmath>
 #include <iostream>
 
 Point::Point(float x, float y) : x(x), y(y) {}
+
+float Point::getX() const { return x; }
+float Point::getY() const { return y; }
 
 void Point::display() const {
     std::cout << "Point(" << x << ", " << y << ")";
 }
 
-float Point::getX() const { return x; }
-float Point::getY() const { return y; }
+bool Point::operator==(const Point& other) const {
+    return (std::abs(x - other.x) < EPSILON && 
+           (std::abs(y - other.y) < EPSILON);
+}
+
+bool Point::operator!=(const Point& other) const {
+    return !(*this == other);
+}
