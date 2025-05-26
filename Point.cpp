6f8f427424fe4bd3ddbfa8516832154309d0babb
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-05-26 14:27:02 +0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,7 +1,6 @@
 #include "Point.h"
 #include <cmath>
-
-constexpr double EPSILON = 1e-9;
+#include <limits>
 
 Point::Point(double x, double y) : x(x), y(y) {}
 
@@ -9,10 +8,9 @@ double Point::getX() const { return x; }
 double Point::getY() const { return y; }
 
 bool Point::operator==(const Point& other) const {
-    return (std::abs(x - other.x) < EPSILON && 
-           (std::abs(y - other.y) < EPSILON);
+    return (std::abs(x - other.x) < std::numeric_limits<double>::epsilon() 
+        && std::abs(y - other.y) < std::numeric_limits<double>::epsilon();
 }
 
 bool Point::operator!=(const Point& other) const {
     return !(*this == other);
-}
