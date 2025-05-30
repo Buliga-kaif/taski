f2a681ebb625bdb44bd7170a774d588bbab573e0
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-05-30 22:15:07 +0300

    Update Tests.cpp

--- Tests.cpp
+++ Tests.cpp
@@ -1,38 +1,67 @@
+#include <gtest/gtest.h>
 #include "Deque.h"
-#include <cassert>
 
-void testEmptyDeque() {
+class DequeTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        dq.push_front(30);
+        dq.push_front(20);
+        dq.push_front(10);
+        dq.push_back(40);
+    }
+
     Deque dq;
-    assert(dq.isEmpty());
-    assert(dq.getSize() == 0);
+};
+
+TEST_F(DequeTest, Initialization) {
+    Deque empty;
+    EXPECT_TRUE(empty.isEmpty());
+    EXPECT_EQ(empty.getSize(), 0);
 }
 
-void testPushPop() {
-    Deque dq;
-    dq.push_front(10);
-    assert(dq.peek_front() == 10);
-    dq.push_back(20);
-    assert(dq.peek_back() == 20);
-    assert(dq.pop_front() == 10);
-    assert(dq.pop_back() == 20);
-    assert(dq.isEmpty());
+TEST_F(DequeTest, PushOperations) {
+    EXPECT_EQ(dq.peek_front(), 10);
+    EXPECT_EQ(dq.peek_back(), 40);
+    EXPECT_EQ(dq.getSize(), 4);
 }
 
-void testInitializerList() {
-    Deque dq = {1, 2, 3};
-    assert(dq.toString() == "[1, 2, 3]");
+TEST_F(DequeTest, PopOperations) {
+    EXPECT_EQ(dq.pop_front(), 10);
+    EXPECT_EQ(dq.pop_back(), 40);
+    EXPECT_EQ(dq.getSize(), 2);
 }
 
-void testCopyAssignment() {
-    Deque dq1 = {5, 6, 7};
-    Deque dq2 = dq1;
-    assert(dq2.toString() == "[5, 6, 7]");
+TEST_F(DequeTest, BoundaryConditions) {
+    Deque empty;
+    EXPECT_THROW(empty.pop_front(), std::out_of_range);
+    EXPECT_THROW(empty.peek_back(), std::out_of_range);
 }
 
-int main() {
-    testEmptyDeque();
-    testPushPop();
-    testInitializerList();
-    testCopyAssignment();
-    return 0;
+TEST_F(DequeTest, CopySemantics) {
+    Deque copy = dq;
+    EXPECT_EQ(copy.toString(), "[10, 20, 30, 40]");
+    
+    copy.pop_front();
+    EXPECT_NE(copy.toString(), dq.toString());
+}
+
+TEST_F(DequeTest, MoveSemantics) {
+    Deque moved = std::move(dq);
+    EXPECT_EQ(moved.toString(), "[10, 20, 30, 40]");
+    EXPECT_TRUE(dq.isEmpty());
+}
+
+TEST_F(DequeTest, StreamOperators) {
+    std::stringstream ss;
+    ss << dq;
+    EXPECT_EQ(ss.str(), "[10, 20, 30, 40]");
+
+    int val;
+    dq >> val;
+    EXPECT_EQ(val, 10);
+}
+
+TEST_F(DequeTest, InitializerList) {
+    Deque init = {1, 2, 3, 4};
+    EXPECT_EQ(init.toString(), "[1, 2, 3, 4]");
 }
