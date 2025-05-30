966010aa16d661506894f210e97335403f3e0418
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-05-30 22:14:33 +0300

    Update Deque.h

--- Deque.h
+++ Deque.h
@@ -22,12 +22,12 @@ private:
 public:
     // Конструкторы и деструктор
     Deque();
-    Deque(const Deque&& other);
+    Deque(const Deque& other);
     Deque(std::initializer_list<int> init);
     ~Deque();
 
     // Операторы
-    Deque& operator=(const Deque& other);
+    Deque& operator=(const Deque&& other);
     Deque& operator<<(int value); // Добавление в конец
     Deque& operator>>(int& value); // Извлечение из начала
 
