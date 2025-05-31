73598a941e74974232606c88d44d50d43986d78f
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-05-31 09:20:47 +0300

    Update Segment.h

--- Segment.h
+++ Segment.h
@@ -1,26 +1,31 @@
+
 #ifndef SEGMENT_H
 #define SEGMENT_H
-#include "PiecewiseLinearApproximation.h"
-#include <stdexcept>
 
+#include "PiecewisetLinearApproximation.h"
+#include <stdexcept>
+#include <limits>
+#include <iostream>
 /**
  * @class Segment
  * @brief Класс для работы с отрезком (наследник аппроксимации).
  */
-class Segment : public PiecewiseLinearApproximation {
+class Segment : public PiecewisetLinearApproximation {
 private:
     static constexpr float EPSILON = std::numeric_limits<float>::epsilon();
 
 public:
     Segment(const Point& p1, const Point& p2);
     Segment(std::initializer_list<Point> points);
-
-    float calculateY(float x) const override;
+    
+    float calculate(float x) const override;
     void shiftLeft(float delta);
-
     
     static Segment readSegment(std::istream& is);
     void display() const;
+    
+    friend std::ostream& operator<<(std::ostream& os, const Segment& s);
+    friend std::istream& operator>>(std::istream& is, Segment& s);
 };
 
 #endif
