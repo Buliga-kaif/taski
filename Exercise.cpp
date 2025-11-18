27993b945684cff20decaffc830215d86d23167b
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-11-18 09:56:30 +0300

    Update Exercise.cpp

--- Exercise.cpp
+++ Exercise.cpp
@@ -1,9 +1,13 @@
-/**
- * @brief Реализация класса Exercise.
- */
-
 #include "Exercise.h"
 
 miit::algebra::Exercise::Exercise(Matrix<int>& mat, Generator* g)
     : matrix(mat), gen(g)
 {}
+
+miit::algebra::Exercise::~Exercise()
+{
+    if (gen != nullptr) {
+        delete gen;
+        gen = nullptr;
+    }
+}
