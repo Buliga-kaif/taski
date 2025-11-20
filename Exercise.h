69839e9d62b9dca255c43d0fb6819b67ebfde25f
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-11-20 10:21:10 +0300

    Update Exercise.h

--- Exercise.h
+++ Exercise.h
@@ -1,43 +1,18 @@
-/**
- * @brief Абстрактный класс для выполнения заданий.
- */
-
 #pragma once
 #include "Matrix.h"
 #include "Generator.h"
 
 namespace miit::algebra
 {
-    /**
-     * @brief Абстрактный класс для выполнения заданий с матрицей и генератором.
-     */
     class Exercise
     {
     protected:
-        Matrix<int> matrix; 
-        Generator* gen;     
+        Matrix<int> matrix;
+        Generator* gen;
 
     public:
-        /**
-         * @brief Конструктор.
-         * @param mat Матрица.
-         * @param g Генератор.
-         */
         Exercise(Matrix<int>& mat, Generator* g);
-
-        /**
-         * @brief Виртуальный деструктор.
-         */
-        virtual ~Exercise() = default;
-
-        /**
-         * @brief Абстрактный метод задания 1.
-         */
-        virtual void Task1() = 0;
-
-        /**
-         * @brief Абстрактный метод задания 2.
-         */
-        virtual void Task2() = 0;
+        virtual ~Exercise();
+        virtual void Task() = 0;
     };
 }
