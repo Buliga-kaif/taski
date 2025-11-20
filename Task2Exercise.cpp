9ad114cfb8df5b07611a0ac9a434854e4c3ef625
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-11-20 10:24:50 +0300

    Update Task2Exercise.cpp

--- Task2Exercise.cpp
+++ Task2Exercise.cpp
@@ -5,13 +5,12 @@
 #include "Task2Exercise.h"
 #include <vector>
 #include <string>
-#include <algorithm>
 
 miit::algebra::Task2Exercise::Task2Exercise(Matrix<int>& mat, Generator* g, int K)
     : Exercise(mat, g), k(K)
 {}
 
-void miit::algebra::Task2Exercise::Task2()
+void miit::algebra::Task2Exercise::Task()
 {
     std::vector<int> result;
     for (size_t i = 0; i < matrix.getSize(); ++i) {
@@ -23,7 +22,6 @@ void miit::algebra::Task2Exercise::Task2()
         result.push_back(val);
     }
 
-    // Пересоздаём матрицу
     Matrix<int> newMat(result.size());
     for (size_t i = 0; i < result.size(); ++i) {
         newMat[i] = result[i];
