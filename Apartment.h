a2209cc2d5314414765eac73bbbb5bfa3c559343
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-12-18 11:22:50 +0300

    Update Apartment.h

--- Apartment.h
+++ Apartment.h
@@ -6,10 +6,11 @@
 
 class Apartment : public Property {
 public:
-    Apartment(const std::string& loc, double a, double p) : Property(loc, a, p) {}
+    Apartment(const std::string& loc, double a, double p)
+        : Property(loc, a, p) {}
 
     std::string getType() const override { return "Apartment"; }
     PropertyType getEnumType() const override { return PropertyType::Apartment; }
 };
 
-#endif
+#endif 
