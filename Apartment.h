314f390f0591d3fa694e95b3dda00e92d21bc7d2
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-12-18 11:03:13 +0300

    Update Apartment.h

--- Apartment.h
+++ Apartment.h
@@ -3,32 +3,13 @@
 #define APARTMENT_H
 
 #include "Property.h"
-#include <string>
 
-/**
- * @brief Класс, представляющий квартиру.
- *
- * Наследуется от Property и реализует его виртуальные методы.
- */
 class Apartment : public Property {
-private:
-    std::string location; ///< Местоположение квартиры.
-    double area;          ///< Площадь квартиры.
-    double price;         ///< Цена квартиры.
-
 public:
-    /**
-     * @brief Конструктор квартиры.
-     * @param loc Местоположение.
-     * @param a Площадь.
-     * @param p Цена.
-     */
-    Apartment(const std::string& loc, double a, double p);
+    Apartment(const std::string& loc, double a, double p) : Property(loc, a, p) {}
 
-    std::string getLocation() const override;
-    double getArea() const override;
-    double getPrice() const override;
-    std::string getType() const override;
+    std::string getType() const override { return "Apartment"; }
+    PropertyType getEnumType() const override { return PropertyType::Apartment; }
 };
 
 #endif
