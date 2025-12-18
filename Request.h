84b68ecfbe7d3aaf14d8a32304f22d42c8076d5e
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-12-18 11:23:40 +0300

    Update Request.h

--- Request.h
+++ Request.h
@@ -10,60 +10,22 @@ enum class RequestType {
     Offer
 };
 
-
 class Request {
 protected:
     std::shared_ptr<Property> property;
-    double price;
+    double price;  
 
 public:
-    Request(std::shared_ptr<Property> prop, double p) : property(prop), price(p) {}
+    Request(std::shared_ptr<Property> prop, double p)
+        : property(std::move(prop)), price(p) {}
+
     virtual ~Request() = default;
 
-    virtual RequestType getEnumType() const = 0; /
+    virtual RequestType getEnumType() const = 0;
     virtual std::string getType() const = 0;
 
     std::shared_ptr<Property> getProperty() const { return property; }
     double getPrice() const { return price; }
 };
 
-/**
- * @brief Абстрактный базовый класс для запросов (спрос и предложение).
- *
- * Содержит ссылку на недвижимость и цену.
- */
-class Request {
-protected:
-    std::shared_ptr<Property> property; ///< Объект недвижимости, связанный с запросом.
-    double price;                       ///< Цена запроса.
-
-public:
-    /**
-     * @brief Конструктор запроса.
-     * @param prop Объект недвижимости.
-     * @param p Цена.
-     */
-    Request(std::shared_ptr<Property> prop, double p);
-
-    virtual ~Request() = default;
-
-    /**
-     * @brief Возвращает тип запроса ("Demand" или "Offer").
-     * @return std::string тип запроса.
-     */
-    virtual std::string getType() const = 0;
-
-    /**
-     * @brief Возвращает связанную недвижимость.
-     * @return std::shared_ptr<Property>.
-     */
-    std::shared_ptr<Property> getProperty() const;
-
-    /**
-     * @brief Возвращает цену запроса.
-     * @return double цена.
-     */
-    double getPrice() const;
-};
-
-#endif
+#endif 
