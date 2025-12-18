77d523cf751b1d59de40a6439f1761530c670b3a
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-12-18 11:25:06 +0300

    Update Demand.h

--- Demand.h
+++ Demand.h
@@ -4,27 +4,12 @@
 
 #include "Request.h"
 
-/**
- * @brief Класс, представляющий спрос на недвижимость.
- *
- * Наследуется от Request и реализует виртуальные методы.
- */
 class Demand : public Request {
 public:
-    /**
-     * @brief Конструктор спроса.
-     * @param prop Объект недвижимости.
-     * @param p Цена, по которой клиент хочет купить.
-     */
-    using Request::Request;  
+    using Request::Request;
 
-    RequestType getEnumType() const override {
-        return RequestType::Demand;
-    }
-
-    std::string getType() const override {
-        return "Demand";
-    }
+    RequestType getEnumType() const override { return RequestType::Demand; }
+    std::string getType() const override { return "Demand"; }
 };
 
 #endif 
