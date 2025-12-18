89440a76fec61bb3ba53cc3c955d46352f5ab257
Плешанов Иван Сергеевич <ivanpleshanov1404@gmail.com>
2025-12-18 11:06:05 +0300

    Update Request.h

--- Request.h
+++ Request.h
@@ -5,6 +5,28 @@
 #include "Property.h"
 #include <memory>
 
+enum class RequestType {
+    Demand,
+    Offer
+};
+
+
+class Request {
+protected:
+    std::shared_ptr<Property> property;
+    double price;
+
+public:
+    Request(std::shared_ptr<Property> prop, double p) : property(prop), price(p) {}
+    virtual ~Request() = default;
+
+    virtual RequestType getEnumType() const = 0; /
+    virtual std::string getType() const = 0;
+
+    std::shared_ptr<Property> getProperty() const { return property; }
+    double getPrice() const { return price; }
+};
+
 /**
  * @brief Абстрактный базовый класс для запросов (спрос и предложение).
  *
